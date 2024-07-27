86a171b7b4687511e36a89a0aecbc132b8943cd4
Orang9 <egbert.fewi@gmail.com>
2024-07-28 02:11:58 +0700

    Fix error

--- utils/db.py
+++ utils/db.py
@@ -1,12 +1,6 @@
 import os
 from pymongo import MongoClient
-from dotenv import load_dotenv
-import bcrypt
 
-# Memuat variabel lingkungan dari file .env
-load_dotenv()
-
-# Ambil URI MongoDB dari environment variable
 mongo_uri = os.getenv("MONGO_URI")
 if not mongo_uri:
     raise ValueError("MONGO_URI environment variable not set")
@@ -20,32 +14,15 @@ chat_collection = db["chat_history"] # Collection to store chat history
 pw_hashing_key = os.getenv("PASSWORD_HASH_KEY")
 def get_user(username, password):
     """Retrieve a user from the database by username and password."""
-    user = users_collection.find_one({"username": username})
-    if user and bcrypt.checkpw(password.encode(), user["password"].encode()):
-        return user
-    return None
+    user = users_collection.find_one({"username": username, "password": password})
+    return user
 
 def add_user(username, password):
     """Add a new user to the database. Return True if successful, else False."""
     if users_collection.find_one({"username": username}):
         return False
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode(), salt).decode()
-    users_collection.insert_one({"username": username, "password": hashed_password})
+    users_collection.insert_one({"username": username, "password": password})
     return True
-
-def fetch_users():
-    users = users_collection.find()
-    user_dict = {"usernames": {}}
-    for user in users:
-        user_dict["usernames"][user["username"]] = {
-            "email": user.get("email", ""),
-            "failed_login_attempts": user.get("failed_login_attempts", 0),
-            "logged_in": user.get("logged_in", False),
-            "name": user.get("name", "Unknown"),
-            "password": str(user["password"])  # Ensure password is a string
-        }
-    return user_dict
 
 def save_session_to_db(session_id, username, session_name, messages):
     """Save the entire session to the MongoDB collection."""
